ab46796d4256c8d99727581eed4ca8a48431b5e3
isantos <isantos@Avenues-MacBook-Pro.local>
2018-06-19 13:05:30 -0300

    class 7

--- src/main/java/javabrains/shapes/Triangle.java
+++ src/main/java/javabrains/shapes/Triangle.java
@@ -2,9 +2,31 @@ package javabrains.shapes;
 
 public class Triangle {
 	private String type;
+	private int height;
+	
+	
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public Triangle() {
+	}
+
+	public Triangle(String type) {
+		this.type = type;
+	}
+	public Triangle(int height,String type) {
+		this.type = type;
+		this.height = height;
+	}
 
 	public void draw() {
-		System.out.println("Draw Triangle");
+		System.out.println("Draw Triangle height:"+height);
 	}
 
 	public String getType() {
@@ -14,5 +36,5 @@ public class Triangle {
 	public void setType(String type) {
 		this.type = type;
 	}
-	
+
 }
