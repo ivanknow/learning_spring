5a941cc8f15feb1df0df68346f9f7cf2f876663a
Ivan Rodrigues <ivanknow@gmail.com>
2018-06-24 21:58:52 -0300

    class 12 - aware

--- src/main/java/javabrains/shapes/Triangle.java
+++ src/main/java/javabrains/shapes/Triangle.java
@@ -2,9 +2,15 @@ package javabrains.shapes;
 
 import java.util.List;
 
-public class Triangle {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware,BeanNameAware{
 	private Style style;
 	private List<Point> points;
+	private ApplicationContext context;
 
 	public Triangle() {
 	}
@@ -31,6 +37,16 @@ public class Triangle {
 		for (Point p : points) {
 			System.out.println(p);
 		}
+	}
+
+	public void setBeanName(String arg0) {
+		System.out.println("Name is "+arg0);
+		
+	}
+
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		this.context = context;
+		
 	}
 
 }
