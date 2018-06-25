c2aed116805ddc90511c01920006de99f02e729b
Ivan Rodrigues <ivanknow@gmail.com>
2018-06-24 21:42:53 -0300

    class 11 - scope

--- src/main/java/javabrains/DrawingApplication.java
+++ src/main/java/javabrains/DrawingApplication.java
@@ -3,6 +3,7 @@ package javabrains;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javabrains.shapes.Style;
 import javabrains.shapes.Triangle;
 
 public class DrawingApplication {
@@ -10,7 +11,15 @@ public static void main(String[] args) {
 	//Triangle t = new Triangle();
 	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 	Triangle t = (Triangle) context.getBean("triangle");
+	Triangle t2 = (Triangle) context.getBean("triangle");
+	Style s = (Style) context.getBean("style");
+	s.setColor("pink");
+	t.draw();
+	
+	t2.getPoints().get(0).setX(100);
 	
 	t.draw();
+	
+	
 }
 }
